21b171213d9caf13209f09b19a8265460077d720
FarrahHamada <68291767+FarrahHamada@users.noreply.github.com>
2020-10-02 23:37:17 -0400

    commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://Userfh:UfkUJE8ev25NFUu@cluster0.cbfyz.gcp.mongodb.net/quickpound?retryWrites=true&w=majority'
+// looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
